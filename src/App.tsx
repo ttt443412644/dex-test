badb9e3cd19da2bcbe2090fd78c86ca8c2f743dc
Alex <john-111@yandex.ru>
2020-01-25 14:59:16 +0200

    modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.tsx 	modified:   src/index.css 	modified:   src/index.tsx 	modified:   yarn.lock
    	db.json
    	json-server.json
    	package-lock.json
    	server/
    	src/api/
    	src/component/
    	src/screen/
    	src/store/
    	src/util/

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,175 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import LoginScreen from "./screen/login/LoginScreen";
+import RegistrationScreen from "./screen/login/RegistrationScreen";
+import ListingScreen from "./screen/listing/ListingScreen";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  RouteProps
+} from "react-router-dom";
+import ProductScreen from "./screen/product/ProductScreen";
+import ProductAddScreen from "./screen/product/ProductAddScreen";
+import PropertyAddScreen from "./screen/property/PropertyAddScreen";
+import { link } from "./util/Constant";
+import Dialog from "./component/dialog/Dialog";
+import { clearNotify, sendNotify } from "./store/helper/actions";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { RootState } from "./store/rootReducer";
+import NotifyDialog from "./component/dialog/NotifyDialog";
+import axios from "axios";
+import { logout } from "./screen/userSlice";
 
 const App: React.FC = () => {
+  const token = localStorage.getItem("cks_token");
+  console.log("token =" + token);
+  if (token) {
+    axios.defaults.headers.common["X-API-Key"] = token;
+  }
+
+  return <AnimationApp />;
+};
+
+function AnimationApp() {
+  const dispatch = useDispatch();
+
+  const { notification, isAuth } = useSelector((state: RootState) => {
+    return {
+      notification: state.helper.notification,
+      isAuth: state.user.isAuthenticated
+    };
+  }, shallowEqual);
+
+  //отлавливаем ошибки
+  axios.interceptors.response.use(
+    response => {
+      return response;
+    },
+    error => {
+      console.log(error);
+      if (error.response.status === 401) {
+        dispatch(logout());
+        dispatch(
+          sendNotify({
+            text: "Ошибка авторизации! Обновите страницу и повторите попытку!",
+            type: "dialog"
+          })
+        );
+      }
+      if (error.response.status === 402) {
+        dispatch(logout());
+        dispatch(
+          sendNotify({
+            text: "Неверные логин и/или пароль!",
+            type: "dialog"
+          })
+        );
+      }
+      if (error.response.status === 403) {
+        dispatch(logout());
+        dispatch(
+          sendNotify({
+            text: "Пользователь с таким email уже существует! ",
+            type: "dialog"
+          })
+        );
+      }
+      return error;
+    }
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path={link.login}>
+            {isAuth ? <Redirect to={link.listing} /> : <LoginScreen />}
+          </Route>
+
+          <Route exact path={link.registration}>
+            {isAuth ? <Redirect to={link.listing} /> : <RegistrationScreen />}
+          </Route>
+
+          <Route exact path="/">
+            <Redirect to={link.listing} />
+          </Route>
+
+          <PrivateRoute path={link.listing} isAuth={isAuth}>
+            <ListingScreen />
+          </PrivateRoute>
+
+          <PrivateRoute exact path={link.productAdd} isAuth={isAuth}>
+            <ProductAddScreen />
+          </PrivateRoute>
+
+          <PrivateRoute exact path={link.productEdit + "/:id"} isAuth={isAuth}>
+            <ProductAddScreen />
+          </PrivateRoute>
+
+          <Route exact path={link.product + "/:id"} isAuth={isAuth}>
+            <ProductScreen />
+          </Route>
+          <PrivateRoute exact path={link.propertyAdd} isAuth={isAuth}>
+            <PropertyAddScreen />
+          </PrivateRoute>
+
+          <Route component={PageNotfound} />
+        </Switch>
+
+        <NotifyDialog
+          content={notification !== null ? notification.text : ""}
+          open={notification !== null && notification.type === "notification"}
+          handleClose={(_: React.SyntheticEvent, reason?: string) => {
+            if (reason === "clickaway") {
+              return;
+            }
+            dispatch(clearNotify());
+          }}
+        />
+
+        <Dialog
+          title="Ошибка"
+          content={notification !== null ? notification.text : ""}
+          open={notification !== null && notification.type === "dialog"}
+          handleClose={() => {
+            dispatch(clearNotify());
+          }}
+        />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+interface PrivateRouteProps extends RouteProps {
+  children?: any;
+  isAuth: boolean;
+}
+
+function PageNotfound() {
+  return <div>404</div>;
+}
+
+function PrivateRoute(props: PrivateRouteProps) {
+  const { isAuth, children, ...rest } = props;
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuth ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: link.login,
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   );
 }
 
